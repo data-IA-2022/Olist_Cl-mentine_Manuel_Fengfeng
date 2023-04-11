6dc045170ed19aaa0819d3f370412e72c232eb86
arscg <arscaldgral@gmail.com>
2023-04-11 17:46:54 +0200

    Test de ORM

---
+++ test.py
@@ -0,0 +1,54 @@
+from olist_model import *
+import json, yaml
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
+
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+print(config)
+engine = create_engine(config['olist'])
+
+
+print('OK')
+ProductCategory
+pc=ProductCategory(product_category_name='clim', product_category_name_english='_en')
+print(pc, pc.is_filled())
+
+pcjson = [pc.to_json()]
+print('en json:', pcjson)
+
+print('Apres modif:')
+pc.set_FR('cat_FR') # Utilisation du setter
+print(pc, pc.is_filled())
+
+
+print('FR:', pc.get_FR())
+
+print('en json:', pc.to_json())
+
+with Session(engine) as session:
+    # session.add(pc)
+    # session.commit()
+
+    # # it = session.query(ProductCategory).filter(text("product_category_name_french is not NULL")).order_by('product_category_name_french')
+    it = session.query(ProductCategory).order_by('product_category_name_french')
+
+    print ()
+    print(it)
+    for pc in it:
+        print(f"  - {pc} {pc.get_FR()}")
+
+    obj = [pc.to_json() for pc in it]
+    print(json.dumps(obj))
+
+#     # print('count', session.query(ProductCategory).count())
+
+
+
+#     # '''pc2 = session.query(ProductCategory).get('bebes')
+#     # print(pc2)
+#     # pc2.set_FR('BB')
+#     # print(pc2.to_json())
+#     # session.commit()'''
+
+
